1319d7ac4babf34e357e9c3d6b59708067e9b11e
Edan Hamilton <edan.hamilton@uq.edu.au>
2024-03-12 10:06:24 +1000

    Move CVMFS to central service

--- setup.py
+++ setup.py
@@ -7,5 +7,7 @@ setup(
   license="MIT",
   url="https://github.com/NeuroDesk/tljh-neurodesk",
   entry_points = {"tljh": ["neurodesk = tljh_neurodesk"]},
-  py_modules=["tljh_neurodesk"]
+  py_modules=["tljh_neurodesk"],
+  packages=["CVMFS"],
+  package_data={"CVMFS": ["*"]},
 )
--- tljh_neurodesk.py
+++ tljh_neurodesk.py
@@ -1,14 +1,20 @@
 from tljh.hooks import hookimpl as hook
+import os
 
 images = ["vnmd/neurodesktop", "jupyter/datascience-notebook"]
 
 @hook
 def tljh_post_install():  # Setup the neurodesk-notebook
-    __import__("os").system("sudo systemctl start docker\n" +
-                '\n'.join([f"sudo docker pull {img}" for img in images]))
+    os    .system("sudo systemctl start docker\n" +
+      '\n'.join([f"sudo docker pull {img}" for img in images]))
+
+    os.chdir(os.path.dirname(__file__) + "/CVMFS")
+    os.replace("apparmor", "/etc/apparmor.d/CVMFS")
+    os.system("""sudo apparmor_parser -r /etc/apparmor.d/CVMFS
+    docker-compose up -d""")
 
 @hook
-def tljh_extra_apt_packages(): return ["docker.io"]
+def tljh_extra_apt_packages(): return ["docker.io", "docker-compose"]
 
 @hook
 def tljh_extra_hub_pip_packages(): return ["dockerspawner", "jupyter_client"]
@@ -20,6 +26,8 @@ def tljh_custom_jupyterhub_config(c):
     c.JupyterHub.spawner_class = "dockerspawner.DockerSpawner"
     c.DockerSpawner.name_template = "{username}-{imagename}"
     c.DockerSpawner.allowed_images = images
-    c.DockerSpawner.extra_host_config = {"privileged": True}
     c.DockerSpawner.extra_create_kwargs = {"user": "root"}
-    c.DockerSpawner.volumes = {"{prefix}-{username}": "/home/jovyan"}
+    c.DockerSpawner.volumes = {
+        "{prefix}-{username}": "/home/jovyan",
+        "/cvmfs": "/cvmfs"
+    }
