328dd20113e49cfd4812f7b970cbb2598226f668
Edan Hamilton <edan.hamilton@uq.edu.au>
2024-03-22 15:46:06 +1000

    Fix incorrect image loading

--- tljh_neurodesk.py
+++ tljh_neurodesk.py
@@ -1,12 +1,12 @@
 from tljh.hooks import hookimpl as hook
 import os
 
-images = ["vnmd/neurodesktop", "jupyter/datascience-notebook"]
+image = "vnmd/neurodesktop"
 
 @hook
 def tljh_post_install():  # Setup the neurodesk-notebook
     os    .system("sudo systemctl start docker\n" +
-      '\n'.join([f"sudo docker pull {img}" for img in images]))
+                  "sudo docker pull image")
 
     if os.path.isdir("/etc/apparmor.d"):
         os.chdir(__import__("CVMFS").__path__[0])
@@ -26,7 +26,7 @@ def tljh_custom_jupyterhub_config(c):
     c.JupyterHub.hub_ip = public_ips()[0]
     c.JupyterHub.spawner_class = "dockerspawner.DockerSpawner"
     c.DockerSpawner.name_template = "{username}-{imagename}"
-    c.DockerSpawner.allowed_images = images
+    c.DockerSpawner.image = image
     c.DockerSpawner.extra_create_kwargs = {"user": "root"}
     c.DockerSpawner.volumes = {
         "{prefix}-{username}": "/home/jovyan",
