c805eafc4c446e079820b12ebd9a24cc508de6f5
UppsalaBrainBehaviorLab <andreas.frick@uu.se>
2024-04-29 10:31:32 +0200

    Test

--- tljh_neurodesk.py
+++ tljh_neurodesk.py
@@ -35,9 +35,16 @@ def tljh_custom_jupyterhub_config(c):
     c.DockerSpawner.volumes = {
         "{prefix}-{username}": "/home/jovyan",
         "/storage/{username}": "/storage/{username}",
-        "storage-{username}": "/data",
+        "/storage-{username}": "/data",
         "/cvmfs": "/cvmfs"
     }
+    c.DockerSpawner.environment = {
+    "CHOWN_HOME": "yes",
+    "CHOWN_EXTRA": "/home/jovyan",
+    "CHOWN_EXTRA_OPTS": "-R",
+    "NB_UID": 1001,
+    "NB_GID": 100,
+}
 
 @hook
 def my_hook(spawner):
