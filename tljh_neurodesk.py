44e751c60afbdb9675212958cd25359734987357
UppsalaBrainBehaviorLab <andreas.frick@uu.se>
2024-04-29 12:24:33 +0200

    Test

--- tljh_neurodesk.py
+++ tljh_neurodesk.py
@@ -34,16 +34,7 @@ def tljh_custom_jupyterhub_config(c):
     c.DockerSpawner.extra_create_kwargs = {"user": "root"}
     c.DockerSpawner.volumes = {
         "{prefix}-{username}": "/home/jovyan",
-        "/storage/{username}": {"bind": "/data", "mode": "rw"},
         "/storage/{username}": "/data",
         "/cvmfs": "/cvmfs"
     }
 
-@hook
-def my_hook(spawner):
-    username = spawner.user.name
-    uid = getpwnam(username).pw_uid
-    spawner.environment['NB_UID'] = uid
-    gid = getpwnam(username).pw_gid
-    spawner.environment['NB_GID'] = gid
-    print(spawner.environment)
